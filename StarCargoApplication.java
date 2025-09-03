5b4e68b1d43322bd271387980292ccbdb7fa5427
del1ght7 <114009947+del1ght7@users.noreply.github.com>
2025-09-03 20:45:28 +0300

    Create StarCargoApplication.java

---
+++ StarCargoApplication.java
@@ -0,0 +1,16 @@
+package com.starcargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for StarCargo website
+ * This class serves as the entry point for the Spring Boot application
+ */
+@SpringBootApplication
+public class StarCargoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarCargoApplication.class, args);
+    }
+}
